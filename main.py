a96671236b949e9d33b64ac0a05ec5c12fc21f84
Shubhendu <shubhenduanupam@gmail.com>
2023-04-01 01:14:14 +0530

    Added full functionality, updated README with limitations of the app, and where the result files are stored. Some improvement can be done.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import filedialog
+from PIL import Image
 
 # ------------------- CONSTANTS ------------------------------------#
 WIDTH = 700
@@ -18,8 +19,65 @@ def select_file(*args):
     file_path = filedialog.askopenfilename()
     image_entry.insert(0, file_path)
 
+
+def get_watermark_size(image_size, watermark_size, scale):
+    img_width, img_height = image_size
+    wm_width, wm_height = watermark_size
+    wm_aspect = wm_width / wm_height
+
+    width = int(img_width * scale)
+    height = int(img_height * scale)
+
+    # TODO: There is some problems in logic here I will correct later, to get good aspect ration of watermark
+    # if img_width < img_height:
+    #     width = int(img_width * scale)
+    #     height = int(width/wm_aspect)
+    # else:
+    #     height = int(img_height * scale)
+    #     width = int(height * wm_aspect)
+    return width, height
+
+
 def watermark(*args):
-    pass
+    watermark_size = size_value.get()
+    watermark_position = value_placement.get()
+    watermark_file = f"watermark.png"
+    image_file = image_entry.get()
+    filename = image_file.rsplit("/", 1)[1].rsplit(".", 1)[0]
+    filename = filename + "_watermarked.png"
+
+    if watermark_size == "Small":
+        watermark_scale = 0.1
+    elif watermark_size == "Medium":
+        watermark_scale = 0.25
+    else:
+        watermark_scale = 0.5
+
+    if image_file:
+        original_image = Image.open(image_file).convert("RGBA")
+        watermark_image = Image.open(watermark_file)
+
+        # Setting the opacity of watermark (0-1)
+        alpha_scale = 0.5
+
+        watermark_size = get_watermark_size(original_image.size, watermark_image.size, watermark_scale)
+        watermark_image = watermark_image.resize(watermark_size)
+
+        # Calculating watermark position,
+        if watermark_position == "Center":
+            watermark_position = (int((original_image.width - watermark_size[0])/2),
+                                  int((original_image.height - watermark_size[1])/2))
+        elif watermark_position == "Bottom Right":
+            watermark_position = (original_image.width - watermark_size[0], original_image.height - watermark_size[1])
+        else:
+            watermark_position = (0, 0)
+
+        mask = watermark_image.split()[3]
+        original_image.paste(watermark_image, watermark_position, mask=mask)
+        original_image.save(f"../{filename}")
+
+        # Informing user that watermarking is done.
+        user_choice.config(text=f"Watermarking Done!!\nFile saved as {filename}", font=("Times New Roman", 20, "bold"), background="green")
 
 # ------------------- USER INTERFACE SETUP ------------------------------#
 window = Tk()
@@ -40,7 +98,7 @@ image_label.grid(row=1, column=0, columnspan=3, pady=5)
 image_entry = Entry(font=FONT, width=60)
 image_entry.grid(row=2, column=0, columnspan=2, pady=5)
 
-icon_img = PhotoImage(file="file_icon_compressed.png")
+icon_img = PhotoImage(file="file_icon.png")
 file_button = Button(width=25, image=icon_img, command=select_file)
 file_button.grid(row=2, column=2, pady=5)
 
@@ -62,7 +120,8 @@ size_options = ["Small", "Medium", "Large"]
 size = OptionMenu(window, size_value, *size_options, command=update_choice)
 size.grid(row=4, column=1, columnspan=2, pady=5)
 
-user_choice = Label(text=f"Watermark will be placed at {value_placement.get()}\n And Size will be {size_value.get()}", font=FONT)
+user_choice = Label(text=f"Watermark will be placed at {value_placement.get()}\n And Size will be {size_value.get()}",
+                    font=FONT)
 user_choice.grid(row=5, column=0, columnspan=3, pady=10)
 
 submit = Button(text="WaterMark", font=("Times New Roman", 14, "bold"), command=watermark, background="lightgreen")
